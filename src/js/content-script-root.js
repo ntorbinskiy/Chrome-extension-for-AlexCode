542c6e67cf9a7d4ee86365dcc5c25f6798f521a5
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-16 22:53:18 +0200

    Import icons to main script file

--- src/js/content-script-root.js
+++ src/js/content-script-root.js
@@ -1,6 +1,10 @@
 import mutationObserver from "./mutationObserver";
 import runApp from "./modules/runApp";
 import connectFonts from "./fonts/connectFonts";
+import "../icons/icon128.png";
+import "../icons/icon48.png";
+import "../icons/icon32.png";
+import "../icons/icon16.png";
 
 connectFonts();
 
