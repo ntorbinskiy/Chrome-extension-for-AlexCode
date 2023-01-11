e171ea8668d10c43e85d5ebdf35e6520fe65f1c5
ntorbinskiy <n.torbinskiy@gmail.com>
2023-01-11 22:12:17 +0200

    Revert "Bug fix"
    This reverts commit c3219ce28b5747f5f0ea36e47be9613650520158.

--- src/js/content-script-root.js
+++ src/js/content-script-root.js
@@ -7,15 +7,14 @@ import "../icons/icon32.png";
 import "../icons/icon16.png";
 
 connectFonts();
-const bootstrapApp = () => {
-  runApp();
-  mutationObserver();
-};
 
-const interval = setInterval(() => {
-  const homeButton = document.querySelector("button.home_btn");
-  if (homeButton) {
-    bootstrapApp();
-    clearInterval(interval);
-  }
-}, 100);
+window.addEventListener(
+  "load",
+  () => {
+    setTimeout(() => {
+      runApp();
+      mutationObserver();
+    }, 700);
+  },
+  false
+);
